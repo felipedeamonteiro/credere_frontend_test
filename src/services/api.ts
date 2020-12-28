2bf1b0a70f96583d0640d4f7f2484ae207c8ce10
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-27 22:38:45 -0300

    Add environment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Connection made with the api
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
