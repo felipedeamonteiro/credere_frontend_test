d4355c5817016e084b9f57838a69edd0f3e01fac
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-27 22:51:32 -0300

    Add header to api connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 // Connection made with the api
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
